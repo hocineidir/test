e28bff312491a13c713f2cf63c86cc9b643b16c4
hocineidir <hocine.idir@telecom-supdaris.eu>
2018-12-28 12:40:35 +0100

    display items

--- src/Controller/ComparatorController.php
+++ src/Controller/ComparatorController.php
@@ -12,8 +12,35 @@ class ComparatorController extends AbstractController
      */
     public function index()
     {
+        
+        $feedIo = \FeedIo\Factory::create()->getFeedIo();
+        
+        $url = 'https://www.careserve.fr/leguide-2-s1-fr-EUR.xml';
+        // read a feed
+        $result = $feedIo->read($url);
+        
+        // or read a feed since a certain date
+        //$result = $feedIo->readSince($url, new \DateTime('-7 days'));
+        
+        $items = array();
+        // iterate through items
+        foreach( $result->getFeed() as $item ) {
+            array_push($items, $item);
+        }
+        
+        $itemsless = array_slice($items,0,10);
+        //$elements = array();
+        //foreach ( $itemsless as $item) {
+            //array_push($elements, $item->getAllElements()->getArrayCopy());
+        //}
+        $elements = $itemsless[0]->getAllElements()->getArrayCopy();
+        
         return $this->render('comparator/index.html.twig', [
-            'controller_name' => 'ComparatorController',
+            'items'  => $itemsless,
+            'elements' => $elements
         ]);
+        
+        
+        
     }
 }
