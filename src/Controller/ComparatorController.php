1b422bf200f3d1c152cf4ed491f0cb5e719469b8
hocineidir <hocine.idir@telecom-supdaris.eu>
2019-01-11 21:49:40 +0100

    knppaginator

--- src/Controller/ComparatorController.php
+++ src/Controller/ComparatorController.php
@@ -4,33 +4,46 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Merchant;
 use App\Entity\Product;
 use Doctrine\ORM\Mapping\Entity;
+use App\Repository\ProductRepository;
 
 class ComparatorController extends Controller
 {
     /**
      * @Route("/comparator", name="comparator")
      */
-    public function index()
+    public function index(Request $request)
     {
        
         $em = $this->getDoctrine()->getManager();
-        $existingproducts = $em->getRepository(Product::class)->findAll();
-        $merchants = $em->getRepository(Merchant::class)->findAll();
+        $allproducts = $em->getRepository(Product::class)->findAll();
+        //$merchants = $em->getRepository(Merchant::class)->findAll();
         
         
         $feedurl = "https://www.careserve.fr/leguide-2-s1-fr-EUR.xml";
         $feed = simplexml_load_file($feedurl);
         
-        $namespace = $feed->getNamespaces(true);
+        /* @var $paginator \Knp\Component\Pager\Paginator */
+        $paginator  = $this->get('knp_paginator');
+        
+        $products = $paginator->paginate(
+            // Doctrine Query, not results
+            $allproducts,
+            // Define the page parameter
+            $request->query->getInt('page', 1),
+            // Items per page
+            60
+            );
+        
+        //$namespace = $feed->getNamespaces(true);
                 
         //$products = array();
         //foreach ($feed->channel->item as $item) {
             //$g = $item->children($namespace["g"]);
             //$product = new Product();
-            //if ( $existingproducts)
             //$product->setTitle($item->title);
             //$product->setDescription($item->description);
             //$product->setLink($item->link);
@@ -42,11 +55,8 @@ class ComparatorController extends Controller
         
         //$em->flush();
         
-        
-        
         return $this->render('comparator/index_comparator.html.twig', [
-            'products' => array_slice($existingproducts, 0, 10),
-            'feed' => $feed
+            'products' => $products,
         ]);   
     }
     
